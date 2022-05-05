c68fd9327e370d75da827895e8c46b5f3223bcfc
Toha92 <ahesan868@gmail.com>
2022-05-06 03:59:12 +0600

    node server connected with mongodb

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
-const app = express()
+const app = express();
 
-// middleware 
-app.use(cors())
-app.use(express.json())
+// middleware
+app.use(cors());
+app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vsva9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    await client.connect();
+    const inventoryCollection = client.db("Warehouse").collection("inventory");
+  } finally {
+  }
+}
 
+run().catch(console.dir);
 
 // root api
-app.get('/', (req, res)=>{
-    res.send('Management server is running')
-})
+app.get("/", (req, res) => {
+  res.send("Management server is running");
+});
 
-
-app.listen(port, ()=>{
-    console.log('Listening to ', port);
-})
+app.listen(port, () => {
+  console.log("Listening to ", port);
+});
